10d4bcd2c9429b60a2ecdb59e85d6df5517caceb
OnixFang <twily.dev@outlook.com>
2021-08-14 23:04:26 -0400

    Setup calendar responsiveness

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Calendar from './Calendar';
 const App = () => {
   return (
     <div className="app">
-      <div>
+      <div className="container">
         <Calendar />
       </div>
     </div>

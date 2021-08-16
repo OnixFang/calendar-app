dbb118bf7dc649da02a0903bea43c6f3979a314c
OnixFang <twily.dev@outlook.com>
2021-08-16 00:17:30 -0400

    Setup reminder sorting

--- src/store/reminders/index.js
+++ src/store/reminders/index.js
@@ -1,23 +1,35 @@
 import * as actions from './types';
 
+const sortFunction = (reminderA, reminderB) => {
+  if (reminderA.time < reminderB.time) {
+    return -1;
+  }
+  if (reminderA.time > reminderB.time) {
+    return 1;
+  }
+
+  return 0;
+};
+
 const remindersReducer = (state = [], action) => {
   switch (action.type) {
     case actions.ADD_REMINDER:
       if (state.find((reminder) => reminder.id === action.payload.id)) {
-        return state.map((reminder) => {
-          if (reminder.id === action.payload.id) {
-            return action.payload;
-          }
-          return reminder;
-        });
+        return state
+          .map((reminder) => {
+            if (reminder.id === action.payload.id) {
+              return action.payload;
+            }
+            return reminder;
+          })
+          .sort(sortFunction);
       }
-      return [...state, action.payload];
+      return [...state, action.payload].sort(sortFunction);
     case actions.DELETE_REMINDER:
       return state.filter((reminder) => reminder.id !== action.payload.id);
     case actions.DELETE_ALL_REMINDERS:
       return state.filter(
-        (reminder) =>
-          reminder.date !== action.payload.toDateString()
+        (reminder) => reminder.date !== action.payload.toDateString()
       );
     default:
       return state;
